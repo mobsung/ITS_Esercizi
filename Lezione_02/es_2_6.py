689e65a7904d409b3d556026ebae409d182cb168
mobsung <movileanu.marcel2001@gmail.com>
2025-02-17 19:29:16 +0100

    commit

---
+++ Lezione_02/es_2_6.py
@@ -0,0 +1,8 @@
+'''2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. 
+Then compose your message and represent it with a new variable called message. Print your message.'''
+
+
+nome:str = "Albert Einstein"
+citazione:str = "A person who never made a mistake never tried anything new."
+
+print(f"{nome} once said, {citazione}")
---
+++ Lezione_02/es_3_2.py
@@ -0,0 +1,8 @@
+'''3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, 
+print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.'''
+
+
+
+names:list = ("Ping", "Proxy")
+for name in names:
+    print(f"{name}: il leggendario.")
