78d7266602620785d5bc03e0800c9540670baf2f
mobsung <movileanu.marcel2001@gmail.com>
2025-02-18 18:37:07 +0100

    commit

--- Lezione_03/es_Mario_Kart.py
+++ Lezione_03/es_Mario_Kart.py
@@ -3,10 +3,10 @@ posizioni:list = ["st", "nd", "rd", "th"]
 posizione:int = int(input("Insert position: "))
 for pos in range(max_pos):
     if pos < 4 and pos == posizione:
-        print(f"Sei arrivato {posizione}{posizioni[pos - 1]}")
+        print(f"You're position is: {posizione}{posizioni[pos - 1]} place.")
         break
     elif pos > 3:
-        print(f"Sei arrivato {posizione}{posizioni[3]}")
+        print(f"You're position is: {posizione}{posizioni[3]} place.")
         break
 
 
