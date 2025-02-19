09c05cbc29f8a5dfa5f82b856a9fea733489f301
mobsung <movileanu.marcel2001@gmail.com>
2025-02-19 18:08:24 +0100

    commit

--- Lezione_02/es_1_5.py
+++ Lezione_02/es_1_5.py
@@ -1,12 +1,12 @@
 '''1-5. Si scriva un programma che converta la temperatura da Fahrenheit a Celsius utilizzando la formula
-gradiCelsius=5∗(gradiFahrenheit−32)/9
+gradiCelsius=5*(gradiFahrenheit-32)/9
 Si inizializzi una temperatura espressa in gradi Fahrenheit con un numero intero.
 La temperatura deve essere convertita e visualizzata in gradi Celsius con un numero in virgola mobile con una precisione di un decimo di grado.
 Un possibile esempio di output potrebbe essere il seguente:
 72 gradi Fahrenheit corrispondono a 22.2 gradi Celsius.'''
 
 
-gradiFahrenheit:int = 72
-gradiCelsius:int = 5 * (gradiFahrenheit - 32) / 9
+gradiFahrenheit:int = int(input("Inserisci il valore in gradi Fahrenheit:"))
+gradiCelsius:float = 5 * (gradiFahrenheit - 32) / 9
 print (f"La temperatura in gradi Celsius equivale a: {gradiCelsius:.2f}°C")
 
