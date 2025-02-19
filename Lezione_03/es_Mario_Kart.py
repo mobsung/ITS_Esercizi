b0ccd1939adaec3274e8e8938cc5933e5593b3db
mobsung <movileanu.marcel2001@gmail.com>
2025-02-19 15:09:10 +0100

    commit

--- Lezione_03/es_Mario_Kart.py
+++ Lezione_03/es_Mario_Kart.py
@@ -1,5 +1,5 @@
-max_pos:int = 20
 posizioni:list = ["st", "nd", "rd", "th"]
+max_pos:int = len(posizioni) + 1
 posizione:int = int(input("Insert position: "))
 for pos in range(max_pos):
     if pos < 4 and pos == posizione:
@@ -9,4 +9,3 @@ for pos in range(max_pos):
         print(f"You're position is: {posizione}{posizioni[3]} place.")
         break
 
-    
