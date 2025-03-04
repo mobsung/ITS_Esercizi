98631f3dd82ddd6fd51268b450cbb537630b78c3
mobsung <movileanu.marcel2001@gmail.com>
2025-03-04 18:16:19 +0100

    commit

---
+++ Lezione_04/es_somme_function.py
@@ -0,0 +1,21 @@
+
+#Funzione somma in range a, b
+def sumInRange(a:int, b:int):
+    sum:int = 0
+    for i in range(a, b + 1):
+        sum += i
+    return sum
+
+#Somma interi da 1 a 10 = 55
+somma:int = sumInRange(1, 10)
+print(f"La somma tra dei numeri interi tra 1 e 10 compresi è pari a {somma}")
+
+
+#Somma interi da 20 a 37 = 513
+somma:int = sumInRange(20, 37)
+print(f"La somma tra dei numeri interi tra 1 e 10 compresi è pari a {somma}")
+
+
+#Somma interi da 35 a 49 = 630
+somma:int = sumInRange(35, 49)
+print(f"La somma tra dei numeri interi tra 1 e 10 compresi è pari a {somma}")
