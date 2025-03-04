f04b26bfc112f59f220952563d494a64682acd0f
mobsung <movileanu.marcel2001@gmail.com>
2025-03-04 17:47:37 +0100

    commit

---
+++ Lezione_04/es_somme_interi.py
@@ -0,0 +1,24 @@
+
+
+#Somma interi da 1 a 10 = 55
+somma:int = 0
+for i in range (1, 11):
+    somma += i
+print(f"La somma tra dei numeri interi tra 1 e 10 è pari a {somma}")
+
+
+#Somma interi da 20 a 37 = 513
+somma:int = 0
+for i in range (20, 38):
+    somma += i
+print(f"La somma tra dei numeri interi tra 20 e 37 è pari a {somma}")
+
+
+
+#Somma interi da 35 a 49 = 630
+somma:int = 0
+for i in range (35, 50):
+    somma += i
+print(f"La somma tra dei numeri interi tra 35 e 49 è pari a {somma}")
+
+
