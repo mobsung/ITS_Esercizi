4e69aba21d8cf9d90b4bd2dc8b5dde6bba63ea1f
mobsung <movileanu.marcel2001@gmail.com>
2025-03-04 19:23:52 +0100

    commit

--- Lezione_04/es_somme_function.py
+++ Lezione_04/es_somme_function.py
@@ -9,13 +9,15 @@ def sumInRange(a:int, b:int):
 #Somma interi da 1 a 10 = 55
 somma:int = sumInRange(1, 10)
 print(f"La somma tra dei numeri interi tra 1 e 10 compresi è pari a {somma}")
+#print(f"Sum from 1 to 10 is {sumInRange(1,10)}")
 
 
 #Somma interi da 20 a 37 = 513
 somma:int = sumInRange(20, 37)
 print(f"La somma tra dei numeri interi tra 20 e 37 compresi è pari a {somma}")
-
+#print(f"Sum from 20 to 37 is {sumInRange(20,37)}")
 
 #Somma interi da 35 a 49 = 630
 somma:int = sumInRange(35, 49)
 print(f"La somma tra dei numeri interi tra 35 e 49 compresi è pari a {somma}")
+#print(f"Sum from 35 to 49 is {sumInRange(35,49)}")
---
+++ Lezione_04/es_substract_function.py
@@ -0,0 +1,20 @@
+
+
+#Funzione sottrazione a - b
+def substract(a, b):
+    result:int = a - b
+    return result
+
+#Esempio 8, 6 = 2
+print(f"La differenza tra 8 e 6 è {substract(8, 6)}")
+
+
+#Esempio 234, 211 = 23
+print(f"La differenza tra 234 e 211 è {substract(234, 211)}")
+
+
+#Esempio 7893, 342324 = -334431
+print(f"La differenza tra 7893 e 342324 è {substract(7893, 342324)}")
+
+
+
