527ee640e5d1d5c9dc7d8794d44fa1950e2ce9ff
mobsung <movileanu.marcel2001@gmail.com>
2025-03-05 17:47:23 +0100

    com

---
+++ Lezione_04/es_Esercizio_1.py
@@ -0,0 +1,12 @@
+'''Write a function check_value(), which takes a number as an argument.
+Using if / else, the function should print whether the number is bigger, smaller, or equal to 5.'''
+
+def check_value(n):
+    if n > 5:
+        print(f"{n} is bigger that 5!")
+    elif n < 5:
+        print(f"{n} is smaller that 5!")
+    else:
+        print(f"{n} is equal to 5!")
+
+check_value(int(input("Digita un numero: ")))
