5e2e8d369f18ec41461ee70f32ce95930fc16a81
mobsung <movileanu.marcel2001@gmail.com>
2025-03-07 15:19:50 +0100

    commit

---
+++ Lezione_03/Esercizi_Match_Statement/es_3C_10.py
@@ -0,0 +1,44 @@
+'''
+Esercizio 3C-10. Scrivere un programma in Python che permetta all'utente di inserire una data (giorno e mese espressi in forma numerica), salvi la data in una tupla e utilizzi un match statement per verificare se la data corrisponde a una festività o a un evento speciale:
+
+- Capodanno → 1 Gennaio → "Capodanno"
+- San Valentino → 14 Febbraio → "San Valentino"
+- Festa della Repubblica Italiana → " Giugno → "Festa della Repubblica Italiana"
+- Ferragosto → 15 Agosto → "Ferragosto"
+- Halloween → 31 Ottobre → "Halloween"
+- Natale → 25 Dicembre → "Natale"
+- Altro caso → "Nessuna festività importante in questa data."
+
+Expected Output:
+
+Inserisci il giorno: 25
+Inserisci il mese: 12
+Output: Il 25/12 è Natale!
+
+- - - - - - - - - - - - - - -
+
+Inserisci il giorno: 5
+Inserisci il mese: 3
+Output: Nessuna festività importante in questa data.
+'''
+
+
+
+data: tuple[int, int] = int(input("Inserisci il giorno:")), int(input("Inserisci il mese:"))
+
+match data:
+    case (1, 1):
+        print(f"Il {data[0]}/{data[1]} è Capodanno!")
+    case (14, 2):
+        print(f"Il {data[0]}/{data[1]} è San Valentino!")
+    case (2, 6):
+        print(f"Il {data[0]}/{data[1]} è la Festa della Repubblica Italiana!")
+    case (15, 8):
+        print(f"Il {data[0]}/{data[1]} è Ferragosto!")
+    case (31, 10):
+        print(f"Il {data[0]}/{data[1]} è Halloween!")
+    case (25, 12):
+        print(f"Il {data[0]}/{data[1]} è Natale!")
+    case _:
+        print("Nessuna festività importante in questa data.")
+    
