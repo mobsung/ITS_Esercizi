d422ffa760d1a3dca1b69e98ed1f7ea18cec3888
mobsung <movileanu.marcel2001@gmail.com>
2025-03-07 17:11:45 +0100

    commit

---
+++ Lezione_04/Esercizi_Funzioni/es_8_7.py
@@ -0,0 +1,23 @@
+'''8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the  dictionaries are storing the album information correctly. Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. Make at least one new function call that includes the number of songs on an album.'''
+
+from typing import Any
+
+def make_album(name:str, album:str, songs_number:int = None):
+    if songs_number is None:
+        mydict: dict[str, Any] = {"Artist name": name, "Album name": album}
+        for key, value in mydict.items():
+            print(f"{key}: {value}", end = " ")
+    else:
+        mydict: dict[str, Any] = {"Artist name": name, "Album name": album, "Number of songs": songs_number}
+        for key, value in mydict.items():
+            if key != "Number of songs":
+                print(f"{key}: '{value}'", end = " - ")
+            else:
+                print(f"{key}: {value}", end = " - ")
+        print("\n")
+        
+
+make_album("Evanescence", "Fallen", 11)
+make_album("Broken Q", "Prelude to Darkness", 10)
+make_album("AC/DC", "Back in Black")
+print("\n")
