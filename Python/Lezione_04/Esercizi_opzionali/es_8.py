6ff4464ebe8264a160c09ff792032b98a253f0c3
mobsung <movileanu.marcel2001@gmail.com>
2025-04-08 10:18:34 +0200

    esercizio 8 opzionale

--- Python/Lezione_04/Esercizi_opzionali/es_8.py
+++ Python/Lezione_04/Esercizi_opzionali/es_8.py
@@ -8,8 +8,63 @@ Provide appropriate feedback to the user for each transaction.
 '''
 
 
+def setAccountBalance() -> float:
+
+    starting_balance = float(input("Set starting balance:\n==>"))
+
+    return starting_balance
 
 
 
+def deposit(balance:float) -> float:
+
+    deposit_amount:float = float(input("What amount would you like to deposit:\n==>"))
+    balance += deposit_amount
+
+    print(f"You deposited {deposit_amount} $ - Your total amount is now {balance} $\n")
+
+    return balance
 
 
+def withdraw(balance:float) -> float:
+    
+    withdraw_amount:float = float(input("What amount would you like to deposit:\n==>\n"))
+    
+    if balance - withdraw_amount < 0:
+        print("You don't have enough money to withdraw\n")
+    else:    
+        balance -= withdraw_amount
+        print(f"You withdrew {withdraw_amount} $ - Your total amount is now {balance} $\n")
+
+    return balance
+
+
+def checkBalance(balance:float):
+
+    print(f"Your balance is: {balance} $\n")
+
+
+def atm(choice:str = ""):
+    
+    balance:float = setAccountBalance()
+
+    while choice != "finish":
+
+        choice = input('Type "deposit" - to DEPOSIT money\n'
+                       'Type "withdraw" - to WITHDRAW money\n'
+                       'Type "check" - to CHECK the balance\n'
+                       'Type "finish" - to STOP\n==>'
+                      )
+        choice.lower()
+
+        if choice == "deposit":
+            balance = deposit(balance)
+
+        if choice == "withdraw":
+            balance = withdraw(balance)
+
+        if choice == "check":
+            checkBalance(balance)
+        
+
+atm()
