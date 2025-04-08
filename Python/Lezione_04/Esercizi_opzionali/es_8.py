233a4c16239fc8fa739ee66c330b0ea85b86b0f5
mobsung <movileanu.marcel2001@gmail.com>
2025-04-08 11:20:39 +0200

    patch es 8

--- Python/Lezione_04/Esercizi_opzionali/es_8.py
+++ Python/Lezione_04/Esercizi_opzionali/es_8.py
@@ -12,8 +12,8 @@ Provide appropriate feedback to the user for each transaction.
 
 def is_float(value) -> bool:
     try:
-        float(value)  # Try to convert the string to a float
-        return True   # If successful, it's a float
+        if float(value) > 0:  # Try to convert the string to a float
+            return True   # If successful, it's a float
     except ValueError:
         return False  # If conversion fails, it's not a float
 
