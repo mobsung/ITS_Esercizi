ed740b04cc565ed644eb0566c0df716cb92253a6
mobsung <movileanu.marcel2001@gmail.com>
2025-04-08 10:26:04 +0200

    added es 9 opzionali

---
+++ Python/Lezione_04/Esercizi_opzionali/es_9.py
@@ -0,0 +1,11 @@
+'''
+9. Caesar Cipher Encryption/Decryption
+
+    Create functions for encrypting and decrypting a message using the Caesar cipher.
+    Allow the user to specify the shift value (number of positions to shift each letter).
+    Handle both encryption and decryption using the same function with appropriate adjustments.
+    Encrypt and decrypt the given message using the specified shift value.
+
+'''
+
+
