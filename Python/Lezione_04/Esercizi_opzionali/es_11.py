a941f7d309e663557b4ee741cf6a43f0b4629c1d
mobsung <movileanu.marcel2001@gmail.com>
2025-04-10 13:41:57 +0200

    es_11 opzionale

---
+++ Python/Lezione_04/Esercizi_opzionali/es_11.py
@@ -0,0 +1,13 @@
+'''
+11. Word Search Puzzle Solver:
+
+    Create a function that solves a word search puzzle.
+    Provide a 2D grid representing the puzzle and a list of words to find.
+    Implement a backtracking algorithm to search for the words in the grid, marking visited cells to avoid repetition.
+    Output the locations of the found words within the grid.
+'''
+
+
+def searchWord():
+
+    pass
