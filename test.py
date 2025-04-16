0f8e4421cc492b38f91d66ea19e79fb8c1a0daa5
mobsung <movileanu.marcel2001@gmail.com>
2025-04-16 19:08:43 +0200

    test

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
 a = {"x0": "y0", "x1": "y1", "x2": "y2"}
-c = a
-
 b = {}
 
-for i in range(len(a)):
-    for key, value in a.items():
-        if i not in b:
-            print(f'{i} - {key} - {value}')
-            b[i] = (key, value)
-       
-        
+for i, (key, value) in enumerate(a.items()):
+    b[i] = (key, value)
+
 print(b)
 
-
-# dict1 = {"z0": "v0", "z1": "v1", "z2": "v2"}
-
-# for key, value in dict1.items():
-
-#     print(key, value)
