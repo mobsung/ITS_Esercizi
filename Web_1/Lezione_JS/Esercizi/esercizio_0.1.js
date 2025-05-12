e132d63dfcf94d31662de8ea233e22bd86d6d37a
mobsung <movileanu.marcel2001@gmail.com>
2025-05-12 19:28:34 +0200

    es js

---
+++ Web_1/Lezione_JS/Esercizi/esercizio_0.1.js
@@ -0,0 +1,13 @@
+/*Si scriva un programma in un file javascript con il
+nome esercizio1_1.js JavaScript che definisce una
+variabile nome che ha come valore il proprio nome
+(es.Mario).
+Il programma deve stampare in output la stringa
+Ciao Mario, utilizzando la variabile definita e
+l’operatore di concatenazione. Eseguire l’esercizio
+nelle due modalità : lato client e lato server.*/
+
+
+let nome = 'Marcel';
+
+console.log('Ciao ' + nome);
