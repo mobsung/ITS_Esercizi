727496baaa1d1600787e9d7ae4404646d1bcef42
mobsung <movileanu.marcel2001@gmail.com>
2025-06-09 19:12:52 +0200

    react

---
+++ Web_1/React/app.js
@@ -0,0 +1,3 @@
+import * as util from './util.js'
+console.log(util.API_KEY)
+console.log(util.SECRET_KEY)
---
+++ Web_1/React/util.js
@@ -0,0 +1,7 @@
+export let API_KEY = "chiave_secreta";
+export const SECRET_KEY="CHIAVE SEGRETA";
+export function draw(a) {
+ 
+
+  return a;
+}
