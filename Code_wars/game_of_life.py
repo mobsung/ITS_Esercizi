3190c608a6620a05989cf38c5ba76cd3042b4efc
mobsung <movileanu.marcel2001@gmail.com>
2025-07-08 15:06:32 +0200

    game of life

--- Code_wars/game_of_life.py
+++ Code_wars/game_of_life.py
@@ -13,21 +13,7 @@ Each cell's neighborhood is the 8 cells immediately around it (i.e. Moore Neighb
 For illustration purposes, 0 and 1 will be represented as ░░ and ▓▓ blocks respectively (PHP: plain black and white squares). You can take advantage of the htmlize function to get a text representation of the universe, e.g.:
 '''
 
-'''
-    def one_glider():
-        do_test([
-            [1,0,0],
-            [0,1,1],
-            [1,1,0]
-        ], 1, [
-            [0,1,0],
-            [0,0,1],
-            [1,1,1]
-        ])
-
-    @test.it('Two gliders')
-    def two_gliders():
-        do_test([
+'''[
             [1,1,1,0,0,0,1,0],
             [1,0,0,0,0,0,0,1],
             [0,1,0,0,0,1,1,1]
@@ -43,13 +29,138 @@ For illustration purposes, 0 and 1 will be represented as ░░ and ▓▓ bloc
             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]
-        ])
+        ]
+
+        [
+            [1,0,0],
+            [0,1,1],
+            [1,1,0]
+        ]
+        [
+            [0,1,0],
+            [0,0,1],
+            [1,1,1]
+        ]
 '''
 
-def get_generation(cells : list[list[int]], generations : int) -> list[list[int]]:
+def check_neighbours(cells: list[list[int]], row: int, col: int) -> int:
+
+    offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    neighbours = 0
+    for dr, dc in offsets:
+        r, c = row + dr, col + dc
+        if 0 <= r < len(cells):
+            if 0 <= c < len(cells[r]):
+                if cells[r][c] == 1:
+                    neighbours += 1
     
+    return neighbours
+
+
+def expanded_cells(cells: list[list[int]]) -> list[list[int]]:
+
+    inverted: list[list[int]] = [[row[i] for row in cells] for i in range(len(cells[0]))]
+
+    if inverted[0].count(1) > 0:
+        inverted.insert(0, [0 for _ in range(len(inverted[0]))])
+    if inverted[-1].count(1) > 0:
+        inverted.append([0 for _ in range(len(inverted[0]))])
+
+    cells: list[list[int]] = [[row[i] for row in inverted] for i in range(len(inverted[0]))]
+
+
+    if cells[0].count(1) > 0:
+        cells.insert(0, [0 for _ in range(len(cells[0]))])
+    if cells[-1].count(1) > 0:
+        cells.append([0 for _ in range(len(cells[0]))])
+
+    return cells
+
+
+def strip_empty(cells: list[list[int]]) -> list[list[int]]:
+
+
+    inverted = list(zip(*cells))
+
+    inverted = [col for col in inverted if any(cell == 1 for cell in col)]
+
+    cells = [list(row) for row in zip(*inverted)]
+    
+    cells = [row for row in cells if any(cell == 1 for cell in row)]
+    
+    if not cells:
+        return []
+    
+    return cells
+
+
+def get_generation(cells: list[list[int]], generations: int) -> list[list[int]]:
+    
+    print('Starting grid:\n')
+    print_cells(cells)
+    print('\n')
     y: int = 0
 
     while y < generations:
+        cells = expanded_cells(cells)
 
-        pass
+        current_evolution: list[tuple[int, int, int]] = []
+
+        for row in range(len(cells)):
+            for col in range(len(cells[row])):
+                if cells[row][col] == 0 and check_neighbours(cells, row, col) == 3:
+                    current_evolution.append((row, col, 1))
+                elif cells[row][col] == 1:
+                    if check_neighbours(cells, row, col) > 3 or check_neighbours(cells, row, col) < 2:
+                        current_evolution.append((row, col, 0))
+
+        for row, col, cel in current_evolution:
+            cells[row][col] = cel
+
+        print(f'Generation: {y+1}\n')
+        print_cells(cells)
+        print('\n')
+        y += 1
+    
+    # return cells
+
+def print_cells(cells: list[list[int]]) -> None:
+    for row in cells:
+        print("".join("⬜" if cell == 1 else "⬛" for cell in row))
+
+# print(check_neighbours([
+#             [1,0,0],
+#             [0,1,1],
+#             [1,1,0]
+#         ], 0, 1))
+
+# print_matrix(get_generation([
+#             [1,0,0],
+#             [0,1,1],
+#             [1,1,0]
+#         ],6))
+
+
+# print_cells(get_generation([
+#             [1,1,1,0,0,0,1,0],
+#             [1,0,0,0,0,0,0,1],
+#             [0,1,0,0,0,1,1,1]
+#         ], 16))
+
+# get_generation([
+#             [0,1,1,0,1,0,1,0,1,1],
+#             [1,1,0,1,0,1,0,1,0,0],
+#             [0,1,0,1,1,0,1,1,1,0],
+#             [0,1,0,1,0,0,1,0,1,1],
+#             [1,0,0,1,0,1,1,1,1,0]
+#         ], 50)
+
+
+get_generation([
+    [1,0,0,0,1],
+    [1,1,0,1,1],
+    [1,0,1,0,1],
+    [0,1,0,1,0],
+    [0,0,1,0,0]
+], 20)
