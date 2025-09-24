c33ffa392af9fb1af4ccee233e35802682628428
mobsung <movileanu.marcel2001@gmail.com>
2025-09-24 11:49:45 +0200

    max area

--- Python/Esercitazioni/container_with_most_wather/max_area.py
+++ Python/Esercitazioni/container_with_most_wather/max_area.py
@@ -8,15 +8,36 @@ Notice that you may not slant the container.
 
 Example 1:
 
-Input: height = [1,8,6,2,5,4,8,3,7]
+Input: heights = [1,8,6,2,5,4,8,3,7]
 Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
 Example 2:
 
-Input: height = [1,1]
+Input: heights = [1,1]
 Output: 1
 
 '''
+
+
+def maxArea(heights: list[int]) -> int:
+
+    max_area = 0
+    lenght: int = 0
+    
+    for i in range(len(heights)):
+        for j in range(lenght + 1, len(heights)):
+            if i != j:
+                lenght = j - i
+                
+                if min(heights[i], heights[j]) * lenght > max_area:
+                    max_area = min(heights[i], heights[j]) * lenght
+        lenght = i
+    return max_area
+
+if __name__ == '__main__':
+
+    print(maxArea(heights = [1,8,6,2,5,4,8,3,7]))
+
 
 
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print('*' * 5.5)
+
+heights = [1,8,6,2,5,4,8,3,7]
+print(max(heights[1], heights[1 + 1]))
