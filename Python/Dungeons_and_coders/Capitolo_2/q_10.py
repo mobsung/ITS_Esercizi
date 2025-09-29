6b9d37fadd3dd1abcffdb6359244d3b7d04d4f71
mobsung <movileanu.marcel2001@gmail.com>
2025-09-29 14:39:26 +0200

    q_10

--- Python/Dungeons_and_coders/Capitolo_2/q_10.py
+++ Python/Dungeons_and_coders/Capitolo_2/q_10.py
@@ -18,11 +18,3 @@ def deep_get(d: dict | list, path: list, default=None):
             d = d[step]
         
     return d
-
-
-print(deep_get({'a':{'b':[10,20]}},['a','b',1]))
-print(deep_get({},['x'],7))
-print(deep_get({'k':[{'z':5}]},['k',0,'z'],0))
-print(deep_get([{'a':1}],[0,'a'],None))
-print(deep_get({'a':{'b':{'c':9}}},['a','b','c']))
-print(deep_get({'a':[1,2]},['a',1]))
