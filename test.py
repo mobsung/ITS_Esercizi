fee5e21ee61d64d4e208774ace05c535fc41b928
mobsung <movileanu.marcel2001@gmail.com>
2025-10-27 14:58:11 +0100

    test

--- test.py
+++ test.py
@@ -1,33 +1,7 @@
-from Python.Utility_python.timer import timer
+a = {'a' : {'b': 4}}
+
+if a.get('b'):
+    print('OK')
 
 
-def decorator(func):
-
-    def wrapper(*args):
-        print('Number one')
-        func(*args)
-        print('Number two')
-    return wrapper
-
-# def say_whee():
-#     print('Whee')
-
-
-# say_whee = decorator(say_whee)
-
-
-# print(say_whee)
-
-@decorator
-def say_whee(a,b):
-    print(a+b)
-
-
-say_whee(5, 4)
-
-@timer
-def say_whee():
-    print('Whee')
-
-
-say_whee()
+print(b)
