850e8a95ea21e56c5ae7bbc3e51a932b726d18f5
mobsung <movileanu.marcel2001@gmail.com>
2025-11-25 16:06:08 +0100

    creata cartella java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,19 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Real Chat");
+		
+		double a = 5;
+		int b = 2;
+		
+		double div = a/b;
+		double mol = a*b;
+		System.out.println(div);
+		System.out.println(mol);
+		
+	}
+
+}
