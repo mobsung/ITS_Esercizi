863a71dd4b8111df4c2b3633fbb53550798ef766
mobsung <movileanu.marcel2001@gmail.com>
2025-12-01 11:52:06 +0100

    aggiunta cartella java

---
+++ java/java_project_01/src/EserciziClassi/MorraCinese.java
@@ -0,0 +1,57 @@
+package EserciziClassi;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MorraCinese {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random r = new Random();
+		
+		
+		while (true) {
+			
+			int nRandom = r.nextInt(0, 3);
+			System.out.println("Digita '0 -> sasso', '1 -> carta', '2 -> forbici'");
+			int utente = sc.nextInt();
+			
+			switch (utente) {
+			case 0:
+				if (utente == 0 && nRandom == 1) {
+					System.out.println("TU: sasso --|-- AI: carta\nHai perso");
+				} else if (utente == 0 && nRandom == 2) {
+					System.out.println("TU: sasso --|-- AI: forbici\nHai vinto");
+				} else {
+					System.out.println("TU: sasso --|-- AI: sasso\nPareggio");
+				}
+				break;
+			case 1:
+				if (utente == 1 && nRandom == 0) {
+					System.out.println("TU: carta --|-- AI: sasso\nHai vinto");
+				} else if (utente == 1 && nRandom == 2) {
+					System.out.println("TU: carta --|-- AI: forbici\nHai perso");
+				} else {
+					System.out.println("TU: carta --|-- AI: carta\nPareggio");
+				}
+				break;
+			case 2:
+				if (utente == 2 && nRandom == 0) {
+					System.out.println("TU: carta --|-- AI: sasso\nHai perso");
+				} else if (utente == 2 && nRandom == 1) {
+					System.out.println("TU: carta --|-- AI: forbici\nHai vinto");
+				} else {
+					System.out.println("TU: carta --|-- AI: carta\nPareggio");
+				}
+				break;
+			}
+			
+			System.out.println("Se vuoi continuare digita 'SI' altrimenti 'NO'");
+			String scelta = sc.next();
+			if (scelta.equalsIgnoreCase("no")) {
+				break;
+			}
+			
+		}
+		sc.close();
+	}
+}
